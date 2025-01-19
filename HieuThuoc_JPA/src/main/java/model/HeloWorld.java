27e6b99a2a7bc785887d2c3cd9a241669c576179
thanhbinh_2004 <119107980+thanhbinh21@users.noreply.github.com>
2025-01-19 16:41:40 +0700

    Update HeloWorld.java

--- HieuThuoc_JPA/src/main/java/model/HeloWorld.java
+++ HieuThuoc_JPA/src/main/java/model/HeloWorld.java
@@ -2,6 +2,8 @@ package model;
 
 public class HeloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
+
     }
 }
